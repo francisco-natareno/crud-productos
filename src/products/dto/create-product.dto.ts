4f832874fe0fd5b6d8e34acc4d124599755bc4a9
Francisco Natareno <fdeleonn2@miumg.edu.gt>
2025-03-31 17:05:37 -0600

    actualizar DTO para la creación de productos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,30 @@
-export class CreateProductDto {}
+import {
+    IsInt,
+    IsPositive,
+    IsString,
+    IsOptional,
+    IsNotEmpty,
+    IsBoolean,
+  } from 'class-validator';
+  
+  export class CreateProductDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+  
+    @IsInt()
+    @IsPositive()
+    price: number;
+  
+    @IsInt()
+    @IsPositive()
+    stock: number;
+  
+    @IsString()
+    @IsOptional()
+    category?: string;
+  
+    @IsBoolean()
+    @IsOptional()
+    isAvailable?: boolean;
+  }
