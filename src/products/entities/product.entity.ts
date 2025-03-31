cef00d2f536b02a1151402e81211f6bc4191f9d4
Francisco Natareno <fdeleonn2@miumg.edu.gt>
2025-03-31 17:06:28 -0600

    actualizar entidad producto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,38 @@
-export class Product {}
+import {
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    DeleteDateColumn,
+    UpdateDateColumn,
+    Column,
+    Entity,
+  } from 'typeorm';
+  
+  @Entity('products')
+  export class Product {
+    @PrimaryGeneratedColumn({ name: 'id' })
+    id: number;
+  
+    @Column({ name: 'name', type: 'varchar', length: 100 })
+    name: string;
+  
+    @Column({ type: 'decimal', precision: 6, scale: 2 })
+    price: number;
+  
+    @Column({ name: 'stock', type: 'smallint' })
+    stock: number;
+  
+    @Column({ name: 'category', type: 'varchar', length: 60 })
+    category: string;
+  
+    @Column({ name: 'is_available' })
+    isAvailable: boolean;
+  
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+  
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date;
+  
+    @DeleteDateColumn({ name: 'deleted_at' })
+    deletedAt: Date;
+  }
