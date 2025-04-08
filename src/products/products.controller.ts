616847293e5aac1ce4ac94ff24b1dac2f5b97c03
Francisco Natareno <fdeleonn2@miumg.edu.gt>
2025-04-08 17:56:09 -0600

    integración de Swagger en el controlador

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,7 +12,9 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Product')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
