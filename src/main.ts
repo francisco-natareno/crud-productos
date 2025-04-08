3d5ec31b5dfdd422bd9958c7a45106fc6f0e5295
Francisco Natareno <fdeleonn2@miumg.edu.gt>
2025-04-08 17:54:11 -0600

    configurar módulo de Swagger para la API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('CRUD Productos API')
+    .setDescription('API CRUD para productos')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
