e4d8ecfbee6cc52764c328438ac9afda4766ee98
Francisco Natareno <fdeleonn2@miumg.edu.gt>
2025-04-08 18:08:38 -0600

    agregar decoradores de Swagger a los DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -6,24 +6,47 @@ import {
     IsNotEmpty,
     IsBoolean,
   } from 'class-validator';
+  import { ApiProperty } from '@nestjs/swagger';
   
   export class CreateProductDto {
+    @ApiProperty({
+      description: 'Product name',
+      example: 'Laptop',
+    })
     @IsString()
     @IsNotEmpty()
     name: string;
-  
+    
+    @ApiProperty({
+      description: 'Product price',
+      example: 999.99,
+    })
     @IsInt()
     @IsPositive()
     price: number;
-  
+    
+    @ApiProperty({
+      description: 'Product stock',
+      example: 50,
+    })
     @IsInt()
     @IsPositive()
     stock: number;
-  
+    
+    @ApiProperty({
+      description: 'Product category',
+      example: 'Electronics',
+      required: false,
+    })
     @IsString()
     @IsOptional()
     category?: string;
-  
+    
+    @ApiProperty({
+      description: 'Product availability',
+      example: true,
+      required: false,
+    })
     @IsBoolean()
     @IsOptional()
     isAvailable?: boolean;
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,28 +1,48 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsString, IsNumber, IsBoolean } from "class-validator";
 
 export class UpdateProductDto{
-
+    @ApiProperty({
+        description: 'Product name',
+        example: 'Laptop',
+        required: false,
+    })
     @IsOptional()
     @IsString()
     name?: string;
-
+    @ApiProperty({
+        description: 'Product price',
+        example: 999.99,
+        required: false,
+    })
     @IsOptional()
     @IsNumber()
     price?: number;
 
+    @ApiProperty({
+        description: 'Product stock',
+        example: 50,
+        required: false,
+    })
     @IsOptional()
     @IsNumber()
     stock?: number;
 
+    @ApiProperty({
+        description: 'Product category',
+        example: 'Electronics',
+        required: false,
+    })
     @IsOptional()
     @IsString()
     category?: string;
 
+    @ApiProperty({
+        description: 'Product availability',
+        example: true,
+        required: false,
+    })
     @IsOptional()
     @IsBoolean()
     isAvailable?: boolean;
-
-    @IsOptional()
-    @IsString()
-    isCompleted?: boolean;
 }
